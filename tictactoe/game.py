106d9977db30cce992a76a5f75ceb3bc6f34fb31
funkylen <funkylen@gmail.com>
2022-09-27 14:21:09 +0300

    Add new logic

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,3 +1,5 @@
+import prompt
+
 SIGN_X = 'X'
 SIGN_O = 'O'
 
@@ -20,6 +22,21 @@ def get_current_sign(old_sign):
     return SIGN_X
 
 
+def get_user_input(message):
+    min_index = 0
+    max_index = 2
+
+    while True:
+        number = prompt.integer(message) - 1
+
+        if min_index <= number <= max_index:
+            return number
+
+
+def check_is_game_end(state):
+    return False
+
+
 def start():
     sign_empty = '-'
 
@@ -29,19 +46,22 @@ def start():
         [sign_empty, sign_empty, sign_empty]
     ]
 
-    print_state(state)
-
     current_sign = get_current_sign(SIGN_O)
-    steps = 9
 
-    while steps > 0:
+    is_game_end = check_is_game_end(state)
+
+    while not is_game_end:
         print(f'Текущий ход: {current_sign}')
 
-        row_number = int(input('Строка: ')) - 1
-        column_number = int(input('Ряд: ')) - 1
+        print_state(state)
+
+        row_number = get_user_input('Строка: ')
+        column_number = get_user_input('Ряд: ')
+
+        if state[row_number][column_number] != sign_empty:
+            print('Поле уже занято. Выберите другое.')
+            continue
 
         state[row_number][column_number] = current_sign
-
-        print_state(state)
 
         current_sign = get_current_sign(current_sign)
